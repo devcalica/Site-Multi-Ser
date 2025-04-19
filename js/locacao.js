9678bc7925fd3247e27c18e85401d953afd02649
Victoria Caroline Bittencourt <devcalica@gmail.com>
2025-04-19 07:18:50 -0300

    Carrossel pronto
    Ajustes finais no carrossel da pagina de locação

--- js/locacao.js
+++ js/locacao.js
@@ -4,24 +4,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const nextBtn = document.querySelector('.next-btn');
     let currentSlide = 0;
 
-    function showSlide(index) {
-        slides.forEach(slide => slide.classList.remove('ativo'));
-        slides[index].classList.add('ativo');
+    function updateSlides() {
+        slides.forEach((slide, index) => {
+            slide.classList.remove('ativo', 'anterior', 'proximo');
+            
+            if (index === currentSlide) {
+                slide.classList.add('ativo');
+            } else if (index === (currentSlide - 1 + slides.length) % slides.length) {
+                slide.classList.add('anterior');
+            } else if (index === (currentSlide + 1) % slides.length) {
+                slide.classList.add('proximo');
+            }
+        });
     }
 
     function nextSlide() {
         currentSlide = (currentSlide + 1) % slides.length;
-        showSlide(currentSlide);
+        updateSlides();
     }
 
     function prevSlide() {
         currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-        showSlide(currentSlide);
+        updateSlides();
     }
 
     // Event listeners
     nextBtn.addEventListener('click', nextSlide);
     prevBtn.addEventListener('click', prevSlide);
+
+    // Inicializar o primeiro slide
+    updateSlides();
 
     // Autoplay
     let autoplay = setInterval(nextSlide, 5000);
